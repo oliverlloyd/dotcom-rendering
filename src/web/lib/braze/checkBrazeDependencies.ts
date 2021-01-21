ea01df97a502ee85547a82fa5cc31662309a4ab4
Tom Wey <tom.wey@theguardian.com>
2021-01-21 11:27:07 +0000

    Refactor this to use a loop over the dependencies
    I chatted with William and discussed some alternatives, including an
    approach which used Promise.allSettled. However I wasn’t happy with the
    version using allSettled (it also isn’t available in the version of node
    we’re on which introduces further friction). I’ve left the allSettled
    version in here commented, but will delete in a subsequent commit.
    
    I think what we’ve got now could be a happy medium between the original
    dependencyChecker work and the if/else approach.

--- src/web/lib/braze/checkBrazeDependencies.ts
+++ src/web/lib/braze/checkBrazeDependencies.ts
@@ -14,10 +14,14 @@ type FailureResult = {
 	data: ResultData;
 };
 
-type DependencyResult = SuccessResult | FailureResult;
+type DependenciesResult = SuccessResult | FailureResult;
 
 type ResultData = { [key: string]: string | boolean };
 
+type DependencyResult = {
+	name: string;
+	value: Promise<string | boolean | null | undefined>;
+};
 const buildFailureResponse = (name: string, value: any, data: ResultData) => ({
 	isSuccessful: false,
 	failureField: name,
@@ -25,72 +29,110 @@ const buildFailureResponse = (name: string, value: any, data: ResultData) => ({
 	data,
 });
 
+const buildDependencies = (
+	isSignedIn: boolean,
+	idApiUrl: string,
+): DependencyResult[] => {
+	return [
+		{
+			name: 'brazeSwitch',
+			value: Promise.resolve(window.guardian.config.switches.brazeSwitch),
+		},
+		{
+			name: 'apiKey',
+			value: Promise.resolve(window.guardian.config.page.brazeApiKey),
+		},
+		{
+			name: 'brazeUuid',
+			value: isSignedIn ? getBrazeUuid(idApiUrl) : Promise.resolve(null),
+		},
+		{
+			name: 'consent',
+			value: hasRequiredConsents(),
+		},
+		{
+			name: 'userIsGuSupporter',
+			value: Promise.resolve(hideSupportMessaging()),
+		},
+		{
+			name: 'isNotPaidContent',
+			value: Promise.resolve(!window.guardian.config.page.isPaidContent),
+		},
+	];
+};
+
 const checkBrazeDependencies = async (
 	isSignedIn: boolean,
 	idApiUrl: string,
-): Promise<DependencyResult> => {
+): Promise<DependenciesResult> => {
+	const dependencies = buildDependencies(isSignedIn, idApiUrl);
+
 	const data: ResultData = {};
 
-	{
-		const name = 'brazeSwitch';
-		const value = window.guardian.config.switches.brazeSwitch;
-		if (!value) {
-			return buildFailureResponse(name, value, data);
-		}
-		data[name] = value;
-	}
+	for (const { name, value } of dependencies) {
+		try {
+			// eslint-disable-next-line no-await-in-loop
+			const result = await value;
 
-	{
-		const name = 'apiKey';
-		const value = window.guardian.config.page.brazeApiKey;
-		if (!value) {
-			return buildFailureResponse(name, value, data);
+			if (result) {
+				data[name] = result;
+			} else {
+				return buildFailureResponse(name, result, data);
+			}
+		} catch (e) {
+			return buildFailureResponse(
+				name,
+				e && e.message ? e.message : e,
+				data,
+			);
 		}
-		data[name] = value;
-	}
-
-	{
-		const name = 'brazeUuid';
-		const value = await (isSignedIn
-			? getBrazeUuid(idApiUrl)
-			: Promise.resolve(null));
-		if (!value) {
-			return buildFailureResponse(name, value, data);
-		}
-		data[name] = value;
-	}
-
-	{
-		const name = 'consent';
-		const value = await hasRequiredConsents();
-		if (!value) {
-			return buildFailureResponse(name, value, data);
-		}
-		data[name] = value;
-	}
-
-	{
-		const name = 'userIsGuSupporter';
-		const value = hideSupportMessaging();
-		if (!value) {
-			return buildFailureResponse(name, value, data);
-		}
-		data[name] = value;
-	}
-
-	{
-		const name = 'isNotPaidContent';
-		const value = !window.guardian.config.page.isPaidContent;
-		if (!value) {
-			return buildFailureResponse(name, value, data);
-		}
-		data[name] = value;
 	}
 
 	return {
 		isSuccessful: true,
 		data,
 	};
+
+	// return Promise.allSettled(dependencies.map((d) => d.value)).then(
+	// 	(results) => {
+	// 		const data: ResultData = {};
+	// 		let failure: { failureField: string; failureData: any } | undefined;
+
+	// 		results.forEach((result, idx) => {
+	// 			if (failure) return;
+
+	// 			const { name } = dependencies[idx];
+
+	// 			if (result.status === 'fulfilled' && result.value) {
+	// 				data[name] = result.value;
+	// 			} else if (result.status === 'fulfilled') {
+	// 				failure = {
+	// 					failureField: name,
+	// 					failureData: result.value,
+	// 				};
+	// 			} else {
+	// 				failure = {
+	// 					failureField: name,
+	// 					failureData: result.reason,
+	// 				};
+	// 			}
+	// 		});
+
+	// 		if (failure) {
+	// 			return {
+	// 				isSuccessful: false,
+	// 				data,
+	// 				failureField: failure.failureField,
+	// 				failureData: failure.failureData,
+	// 			};
+	// 		}
+
+	// 		return {
+	// 			isSuccessful: true,
+	// 			data,
+	// 		};
+	// 	},
+	// );
 };
 
 export { checkBrazeDependencies };

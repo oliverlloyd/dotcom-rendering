5a979d593a7bfc5faf295a76f3b9d5d37d24f467
Wai Sing Yiu <wai.sing.yiu@guardian.co.uk>
2021-12-01 11:20:31 +0000

    Update the cdk

--- apps-rendering/cdk/lib/mobile-apps-rendering.ts
+++ apps-rendering/cdk/lib/mobile-apps-rendering.ts
@@ -1,8 +1,11 @@
-import { join } from 'path';
 import { CfnInclude } from '@aws-cdk/cloudformation-include';
-import type { App } from '@aws-cdk/core';
+import { App } from '@aws-cdk/core';
+import { AccessScope, GuEc2App } from '@guardian/cdk';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack, GuStageParameter } from '@guardian/cdk/lib/constructs/core';
+import { join } from 'path';
+import { InstanceClass, InstanceSize, InstanceType, Peer, Port } from "@aws-cdk/aws-ec2";
+import { Stage } from "@guardian/cdk/lib/constants";
 
 export class MobileAppsRendering extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
@@ -18,5 +21,34 @@ export class MobileAppsRendering extends GuStack {
 				Stage: GuStageParameter.getInstance(this),
 			},
 		});
+    new GuEc2App(this, {
+      applicationPort: 1234,
+      app: "app-name",
+      access: { scope: AccessScope.PUBLIC },
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
+      certificateProps:{
+        [Stage.CODE]: {
+          domainName: "code-guardian.com",
+          hostedZoneId: "id123",
+        },
+        [Stage.PROD]: {
+          domainName: "prod-guardian.com",
+          hostedZoneId: "id124",
+        },
+      },
+      monitoringConfiguration: {
+        snsTopicName: "alerts-topic-for-my-team",
+        http5xxAlarm: {
+          tolerated5xxPercentage: 1,
+        },
+        unhealthyInstancesAlarm: true,
+      },
+      userData: {
+        distributable: {
+          fileName: "app-name.deb",
+          executionStatement: `dpkg -i /app-name/app-name.deb`,
+        }
+      },
+    });
 	}
 }

5c90a3b74819d82730c2ad829c3436373b03dd97
Ioanna Kokkini <ioannakok@hotmail.com>
2022-06-02 17:16:23 +0100

    wip: start adding state for the button

--- dotcom-rendering/src/web/components/ShowMore.importable.tsx
+++ dotcom-rendering/src/web/components/ShowMore.importable.tsx
@@ -1,4 +1,12 @@
-import { Button, SvgPlus } from '@guardian/source-react-components';
+import {
+	Button,
+	SvgPlus,
+	SvgMinus,
+	SvgSpinner,
+} from '@guardian/source-react-components';
+import { neutral, remSpace } from '@guardian/source-foundations';
+import { css } from '@emotion/react';
+import { useState } from 'react';
 
 /**
  * addDoc
@@ -29,15 +37,56 @@ export const ShowMore: React.FC<{
 	id: string;
 	displayName: string;
 }> = ({ displayName }) => {
-	return (
-		<Button
-			priority="primary"
-			size="small"
-			icon={<SvgPlus />}
-			iconSide="left"
-			// onClick={TODO: handleClick}
-		>
-			More {displayName}
-		</Button>
-	);
+	const [isLoading, setLoading] = useState(false);
+	const [isDataFetched, setDataFetched] = useState(false);
+	const [showLess, setShowLess] = useState(false);
+
+	if (isDataFetched && !showLess) {
+		return (
+			// somewhere here we'll add the extra cards
+			<Button
+				size="xsmall"
+				icon={<SvgMinus />}
+				onClick={() => setShowLess(false)}
+			>
+				Less
+			</Button>
+		);
+	} else if (isLoading) {
+		return (
+			<Button
+				size="xsmall"
+				icon={<SvgSpinner />}
+				iconSide="left"
+				cssOverrides={css`
+					background-color: ${neutral[0]};
+					margin-top: ${remSpace[1]};
+					margin-left: ${remSpace[1]};
+				`}
+			>
+				Loading...
+			</Button>
+		);
+	} else {
+		return (
+			<Button
+				size="xsmall"
+				icon={<SvgPlus />}
+				iconSide="left"
+				cssOverrides={css`
+					background-color: ${neutral[0]};
+					margin-top: ${remSpace[1]};
+					margin-left: ${remSpace[1]};
+				`}
+				onClick={() => {
+					setLoading(true);
+					setTimeout(() => 3000); // Mimicking fetching time. Will be replaced.
+					setDataFetched(true);
+					setShowLess(true);
+				}}
+			>
+				More {displayName}
+			</Button>
+		);
+	}
 };
